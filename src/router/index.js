d86e3ac7c1a3d90e5dd648095ffbea718f1d0358
soze2k <dlii0032@student.monash.edu>
2023-06-07 16:41:25 +1000

    update homepage and index file

--- src/router/index.js
+++ src/router/index.js
@@ -13,14 +13,30 @@ const routes = [
     {
       path: '/',
       name: 'index',
-      redirect: "/homepage",
+      redirect: "/HomePage",
       children: [
         {
-          path: 'homepage',
-          name: 'homepage',
-          component: () => import('@/view/homepage.vue'),
+          path: 'HomePage',
+          name: 'HomePage',
+          component: () => import('@/view/HomePage.vue'),
         },
       ]
     },
 
   ]
+  const router = new VueRouter({
+    mode: 'history',
+    scrollBehavior(to, from, savedPosition) {
+      
+      if (savedPosition) {
+      
+        return savedPosition
+      } else {
+        return { x: 0, y: 0 }
+      }
+    },
+    base: process.env.BASE_URL,
+    routes
+  })
+  
+  export default router
--- src/view/HomePage.vue
+++ src/view/HomePage.vue
@@ -1,7 +1,5 @@
 <template>
     <div id="app">
-      <img alt="Vue logo" src="./assets/logo.png">
-      <h1>'Welcome to Your Vue.js App'</h1>
     </div>
   </template>
   
@@ -15,5 +13,6 @@
     text-align: center;
     color: #2c3e50;
   }
+
   </style>
   
