ed98cc8a0fa09de70b3d6bad3c4b05eb2abe0a69
soze2k <dlii0032@student.monash.edu>
2023-06-07 19:35:47 +1000

    Initial layout builded

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,18 +1,53 @@
 <template>
-    <div id="app">
+  <div id="drag-drop-area">
+    <input type="file" id="file" @change="previewImage" ref="fileUpload" style="display: none" />
+    <div id="drop-area" @drop="dropHandler" @dragover.prevent>
+      <p>Drug and drop your file here</p>
+      <button @click="triggerFileUpload">Choose File</button>
     </div>
-  </template>
-  
-  <script>
+    <img id="preview" :src="image" v-if="image" />
+    <button @click="goToImagePage">Image Library</button>
+  </div>
+</template>
 
-  </script>
-  
-  <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    text-align: center;
-    color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {
+      image: null,
+    }
+  },
+  methods: {
+    triggerFileUpload() {
+      this.$refs.fileUpload.click()
+    },
+    dropHandler(e) {
+  const file = e.dataTransfer.files[0]
+  if (file) {
+    this.createImage(file)
   }
+},
+previewImage(e) {
+  const file = e.target.files[0]
+  if (file) {
+    this.createImage(file)
+  }
+},
+createImage(file) {
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    this.image = e.target.result
+  }
+  reader.readAsDataURL(file)
+},
 
-  </style>
-  
+    goToImagePage() {
+      this.$router.push('/image')
+    },
+  },
+}
+</script>
+
+<style scoped>
+/* ... */
+</style>
