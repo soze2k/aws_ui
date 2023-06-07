64897f3c0c86b27d78ef3912db54c017daa2c86b
soze2k <dlii0032@student.monash.edu>
2023-06-07 16:15:10 +1000

    Create 2 pages

---
+++ src/view/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: 'Welcome to Your Vue.js App',
+      };
+    },
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+  }
+  </style>
+  
