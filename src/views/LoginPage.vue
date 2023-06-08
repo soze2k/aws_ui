0066b44d2ce44ac161fdf86c8d5de97af633ca0f
soze2k <dlii0032@student.monash.edu>
2023-06-08 23:16:16 +1000

    Update LoginPage.vue

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -9,13 +9,12 @@
   export default {
     data() {
       return {
-        loginURL: 'https://cloudsnapgroup15.auth.ap-southeast-2.amazoncognito.com',
+        loginURL: 'https://cloudsnapgroup15.auth.ap-southeast-2.amazoncognito.com/login?client_id=78acqta94st5tcnes8s3c66s94&response_type=token&scope=email+openid&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Flogged_in.html',
       };
     },
     mounted() {
-      
       const script = document.createElement('script');
-      script.src = 'https://cdn.cognitoforms.com/scripts/xxxxxx/xxxxxx.js'; 
+      script.src = 'https://cloudsnapgroup15.auth.ap-southeast-2.amazoncognito.com/login?client_id=78acqta94st5tcnes8s3c66s94&response_type=token&scope=email+openid&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Flogged_in.html';
       script.async = true;
       document.body.appendChild(script);
     },
