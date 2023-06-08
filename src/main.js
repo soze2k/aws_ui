72b27316c7d45bda33bb363c7a7ddc5403c94fe5
soze2k <dlii0032@student.monash.edu>
2023-06-08 21:22:04 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Amplify from 'aws-amplify';
+import AmplifyVue from '@aws-amplify/ui-vue';
 import awsconfig from './aws-exports';
+import '@aws-amplify/ui-vue/styles.css';
 
 Amplify.configure(awsconfig);
 
 createApp(App).use(router).mount('#app')
+
+const app = createApp(App);
+app.use(AmplifyVue);
+app.mount('#app');
